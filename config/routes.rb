223b6b5129ed2ff544e241071663cf94036fcb78
Syed Muaz <smuaz@yahoo.com>
2015-10-01 10:57:57 +0800

    done week1 assignments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Myflix::Application.routes.draw do
+  root to: "pages#front"
+  get 'register', to: "users#new"
+  get 'sign_in', to: "sessions#new"
+  get 'sign_out', to: "sessions#destroy"
+  get 'home', to: 'videos#index'
+
   get 'ui(/:action)', controller: 'ui'
+
+  resources :videos, only: [:show] do
+    collection do
+      post :search, to: 'videos#search'
+    end
+  end
+  resources :categories, only: [:show]
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+commedies = Category.create(name: "TV Commedies")
+tvdramas = Category.create(name: "TV Dramas")
+realitytv = Category.create(name: "Reality TV")
+
+Video.create(title: 'Family Guy', description: 'a funny comedy show', small_cover_url: 'family_guy.jpg', large_cover_url: 'monk_large.jpg', category: commedies)
+Video.create(title: 'Futurama', description: 'a funny space show', small_cover_url: 'futurama.jpg', large_cover_url: 'monk_large.jpg', category: commedies)
+Video.create(title: 'South Park', description: 'an awesome funny show', small_cover_url: 'south_park.jpg', large_cover_url: 'monk_large.jpg', category: commedies)
+Video.create(title: 'Monk', description: 'a drama funny show', small_cover_url: 'monk.jpg', large_cover_url: 'monk_large.jpg', category: tvdramas)
+Video.create(title: 'Family Guy', description: 'a funny comedy show', small_cover_url: 'family_guy.jpg', large_cover_url: 'monk_large.jpg', category: commedies)
+Video.create(title: 'Futurama', description: 'a funny space show', small_cover_url: 'futurama.jpg', large_cover_url: 'monk_large.jpg', category: commedies)
+Video.create(title: 'South Park', description: 'an awesome funny show', small_cover_url: 'south_park.jpg', large_cover_url: 'monk_large.jpg', category: commedies)
+Video.create(title: 'Monk', description: 'a drama funny show', small_cover_url: 'monk.jpg', large_cover_url: 'monk_large.jpg', category: tvdramas)
+Video.create(title: 'South Park', description: 'an awesome funny show', small_cover_url: 'south_park.jpg', large_cover_url: 'monk_large.jpg', category: commedies)
